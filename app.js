d770f4258453c5b7867d4b0565c90e0aa4cb4d55
gunn <gunnkeem@icloud.com>
2021-09-27 22:51:52 +0900

    set env var : NODE_ENV

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const express = require("express");
+const errorHandler = require("errorhandler");
 const path = require('path');
 const cors = require("cors");
 
 
-// ---------------- APP SETTING
+// ---------------- EXPRESS APP SETTING
 const app = express();
-const static = express.static(path.join(__dirname, 'public'));
-
 app.use(express.json());
 app.use(express.text({ defaultCharset: "utf-8" }));
 app.use(express.urlencoded({ extended: true })); // parse requests of content-type - application/x-www-form-urlencoded
-
 app.use(cors({ origin: "http://localhost:8081" }));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }))
+}
+if (process.env.NODE_ENV === "production") {
+  app.use(errorHandler())
+}
+
+
+
+// ---------- SERVE
+console.log(`[==== SERVICE RUN LEVEL : ${process.env.NODE_ENV} ====]`);
+const static = express.static(path.join(__dirname, 'public'));
 app.use(static)
 
 
+
+
+
 // ---------- PORT SETTING
-const PORT = process.env.CLI_PORT || 8080;
+const PORT = (process.env.NODE_ENV === "production") ? 1080 : 8080;
 app.listen(PORT, () => {
-  console.log(`=== Listening on port ${PORT} ... @app.js`);
+  console.log(`[==== Listening on port ${PORT} ... @app.js ====]`);
 });
-
-
-
-
-
-
-
-
-// deprecated - https://www.bezkoder.com/serve-vue-app-express/
-// simple route
-// app.get("/", (req, res) => {
-//  res.json({ message: "Welcome to bezkoder application." });
-// });
-
